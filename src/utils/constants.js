d8767b1fe55d8b844a7fd7dda9d1da76c590f956
Masudha Meher <kasturimeh@gmail.com>
2025-09-11 11:06:29 +0530

    add: movie slice in reducers

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,10 @@
 export const LOGO =
   'https://images.ctfassets.net/y2ske730sjqp/821Wg4N9hJD8vs5FBcCGg/9eaf66123397cc61be14e40174123c40/Vector__3_.svg?w=460';
+
+  export const API_OPTIONS = {
+     method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${process.env.PARCEL_API_KEY}`
+  }
+  }
